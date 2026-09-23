414fcf46ebd09e3190601bd5b16c35a61023be04
Prajjwal Mathur <pra90mathur@gmail.com>
2026-09-23 21:26:36 +0530

    initial setup

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,17 +1,17 @@
 // This file contains all the basic configuration logic for the app server to work
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
 type ServerConfig = {
-    PORT: number
-}
+  PORT: number;
+};
 
 function loadEnv() {
-    dotenv.config();
-    console.log(`Environment variables loaded`);
+  dotenv.config();
+  console.log(`Environment variables loaded`);
 }
 
 loadEnv();
 
 export const serverConfig: ServerConfig = {
-    PORT: Number(process.env.PORT) || 3001
+  PORT: Number(process.env.PORT) || 3002,
 };
